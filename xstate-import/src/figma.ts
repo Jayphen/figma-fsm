1f1f4c618681ca953127aab9d6903cf2b2395d86
Jayphen <jayphen@hyperlab.se>
2020-02-15 21:36:48 +0100

    Increase the width/height of the plugin window

--- xstate-import/src/figma.ts
+++ xstate-import/src/figma.ts
@@ -6,23 +6,33 @@
 // full browser enviroment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, {
+  width: 600,
+  height: 400
+});
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = async msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === "create-rectangles") {
-    const count = valueOrOne(msg.count);
+  if (msg.type === "create-frames") {
+    const value = JSON.parse(msg.value);
+    console.log(value);
+    const MACHINE_NAME = value.id;
+
+    createAndSwitchToPage();
+
     const spacing = valueOrOne(msg.spacing);
 
-    const nodes = createRectangles(count, spacing);
+    const frames = createArtboards(value.states, spacing, {
+      prefix: MACHINE_NAME
+    });
 
-    const group = figma.group(nodes, figma.currentPage, 0);
-    figma.currentPage.selection = [group];
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    const debugFrame = await createDebugArtboard(value);
+
+    figma.viewport.scrollAndZoomIntoView([...frames, debugFrame]);
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
